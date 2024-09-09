1128833061eadcddfde59848a6623faa93dec746
CameroJI <137007386+CameroJI@users.noreply.github.com>
2024-09-09 15:47:50 -0600

    Update base.py

--- interfaces/base.py
+++ interfaces/base.py
@@ -183,8 +183,9 @@ class TextBase(object):
                 if self.config.TRAIN.ngpu == 1:
                     model.load_state_dict(torch.load(self.resume)['state_dict_G'])
                 else:
-                    model.load_state_dict(
-                        {'module.' + k: v for k, v in torch.load(self.config.TEST.checkpoint)})
+                    model.load_state_dict(torch.load(self.config.TEST.checkpoint))
+                    # model.load_state_dict(
+                    #     {'module.' + k: v for k, v in torch.load(self.config.TEST.checkpoint)})
 
         para_num = get_parameter_number(model)
         self.logging.info('Total Parameters {}'.format(para_num))
